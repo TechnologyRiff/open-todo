5e984f7e9f47853fcb1b79d7664299f7f8dafee3
technologyRiff <technologyriff@gmail.com>
2015-03-14 13:57:34 -0400

    updated database

--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Api::ItemsController do 
-  pending # You know what to do
+  describe "create" do
+      xit "takes a name, creates it if it doesn't exist, and returns false if it does"
+    end
 end
